b09e0ef40272195c282df24f84e556dd5062a9a4
vinod kumar <vinodrao835@gmail.com>
2025-11-22 08:25:42 +0530

    Dummy

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -6,22 +6,12 @@ const wss = new WebSocketServer({
 });
 
 wss.on('connection', (ws, req) => {
-    // 1st way
-    //    console.log(typeof url.parse(req.url!, true).query); 
-    //    const value = url.parse(req.url!, true).query;
-    //    console.log(value.token);
-
-
-    // if you want to work with url query parameters
     const url = req.url;
 
     if (!url){
         return;
     }
 
-    console.log(url); // /?token=abacaa&key=1111
     const paramurl= url.split('?')[1];
-    console.group(paramurl) // token=abacaa&key=1111
     const queryParams = new URLSearchParams(paramurl);
-    console.log(queryParams); // { 'token' => 'abacaa', 'key' => '1111' }
 });
