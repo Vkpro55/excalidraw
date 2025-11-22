3edc8c06f10c7eb571904789b587a1150fa21e95
vinod kumar <vinodrao835@gmail.com>
2025-11-22 09:08:39 +0530

    dummy

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,5 +1,6 @@
 import WebSocket, { WebSocketServer } from 'ws';
 import url from "url";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 const wss = new WebSocketServer({
     port: 8080
@@ -7,11 +8,17 @@ const wss = new WebSocketServer({
 
 wss.on('connection', (ws, req) => {
     const url = req.url;
-
     if (!url){
         return;
     }
 
     const paramurl= url.split('?')[1];
     const queryParams = new URLSearchParams(paramurl);
+    const token = queryParams.get("token") || "";
+
+    const decoded = jwt.verify(token, "SECRET");
+    if (!decoded || !(decoded as JwtPayload).userId) {
+        ws.close();
+        return;
+    }
 });
